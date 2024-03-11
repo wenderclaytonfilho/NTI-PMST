b4133680d628bc43d8e9def78980aefc2c59774a
Tobias Alves <tobiasjose12@gmail.com>
2024-03-11 10:26:16 -0300

    up

--- src/main/java/com/estagio/estagio/controller/controller.java
+++ src/main/java/com/estagio/estagio/controller/controller.java
@@ -1,14 +1,19 @@
 package com.estagio.estagio.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
-@RestController
+@Controller
 public class controller {
-    @GetMapping(value = "/")
-    public String getMethodName() {
-        return "oi";
+
+    @GetMapping("/home")
+    public ModelAndView home() {
+    ModelAndView mv = new ModelAndView("index");
+       return mv;
+
     }
 
 }
