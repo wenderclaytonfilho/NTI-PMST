3b7cfcb9fb99109884f02919db09ffb66e512600
Tobias Alves <tobiasjose12@gmail.com>
2024-03-12 09:57:17 -0300

    register

--- src/main/java/com/estagio/estagio/controller/controller.java
+++ src/main/java/com/estagio/estagio/controller/controller.java
@@ -1,10 +1,10 @@
 package com.estagio.estagio.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class controller {
