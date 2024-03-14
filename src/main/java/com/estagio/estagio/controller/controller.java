d488d8e6687a5e8b906b46fa86a05bb715244398
Tobias Alves <tobiasjose12@gmail.com>
2024-03-14 20:39:46 -0300

    up

--- src/main/java/com/estagio/estagio/controller/controller.java
+++ src/main/java/com/estagio/estagio/controller/controller.java
@@ -1,19 +1,14 @@
 package com.estagio.estagio.controller;
 
-
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class controller {
 
-    @GetMapping("/home")
-    public ModelAndView home() {
-    ModelAndView mv = new ModelAndView("tasks");
-       return mv;
+    @GetMapping("/tasks")
+    public String tasks() {
+        return "tasks";
 
     }
-
 }
