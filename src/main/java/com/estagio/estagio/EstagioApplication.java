f5e5a630f19060466d26154c94fb8df5aaa5f201
wenderclaytonfilho <rednew2727@gmail.com>
2024-03-15 09:16:46 -0300

    Estágio

--- src/main/java/com/estagio/estagio/EstagioApplication.java
+++ src/main/java/com/estagio/estagio/EstagioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EstagioApplication {
 
 	public static void main(String[] args) {
+		//Run
 		SpringApplication.run(EstagioApplication.class, args);
 	}
 
